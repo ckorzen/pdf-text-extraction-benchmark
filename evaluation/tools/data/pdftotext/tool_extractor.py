cc310121632ecc9ffc5e0305471668f486e14206
ckorzen <ckorzen@users.noreply.github.com>
2017-09-04 15:49:17 +0200

    Added missing config for pdftotext

---
+++ evaluation/tools/data/pdftotext/tool_extractor.py
@@ -0,0 +1,12 @@
+import sys
+import os.path
+
+# The current working directory.
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(CWD, "../../bin")))
+
+from extractor import ToolExtractor  # NOQA
+
+
+class PdfToTextExtractor(ToolExtractor):
+    pass
